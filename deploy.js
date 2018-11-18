d97be6501654968a505ed1f3bcdb8027f5ce9096
damianjnc <damianjnc@gmail.com>
2018-11-18 09:37:55 +0000

    first commit

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'super youth wet sugar vacant power hub payment book drip false walk',
+  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: '0x' + bytecode })
+        .send({ from: accounts[0] });
+
+
+
+  console.log(interface);
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
